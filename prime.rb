b392790b12a43fdf43643868c34136e54f4fcf5a
Syd Bailey <sydabailey@gmail.com>
2019-07-29 04:58:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(n)
-    return false if n < 2 
-    array = (2..n-1).to_a 
+    return false if n < 2
+    array = (2..n-1).to_a
     array.each do |x|   
         if n % x == 0
             return false

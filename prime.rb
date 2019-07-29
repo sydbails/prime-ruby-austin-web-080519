1d9d907612be6f9fa35e1e82edc3f82da522e113
Syd Bailey <sydabailey@gmail.com>
2019-07-29 04:59:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+    return false if n < 2
+    array = (2..n-1).to_a
+    array.each do |x|   
+        if n % x == 0
+            return false
+        end
+    end
+    true
+end
